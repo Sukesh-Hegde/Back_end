1076eb91972a8f4e40c4374ed236e4d8293bbab4
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-01-25 18:12:30 +0530

    updated
    update

--- E-comm Project using REST API/server.js
+++ E-comm Project using REST API/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import swagger from 'swagger-ui-express';//importing swagger
+import cors from 'cors'; 
 import productRouter from './src/features/product/product.routes.js';
 import userRouter from './src/features/user/user.routes.js';
 // import basicAuthorizer from './middlewares/basicAuth.middleware.js'; //importing authorizer
@@ -8,11 +9,21 @@ import jwtAuth from './middlewares/jwt.middleware.js'; //importing authorizer
 import bodyParser from 'body-parser';
 import CartRouter from './src/features/cardItems/cartItems.routes.js';
 import apiDocs from './swagger.json' assert{type:'json'};//assert= informing this is the json object
+import loggerMiddleware from './middlewares/logger.middleware.js';
 const server = express();
 server.use(bodyParser.json());
 
+// CORS policy configuration
+var corsOptions = {
+    origin: "http://localhost:51370"
+}
+server.use(cors(corsOptions));
+
 //swagger
 server.use("/api-docs",swagger.serve,swagger.setup(apiDocs));
+
+//L ogger midleware
+server.use(loggerMiddleware);
 
 //for all requests related to product, redirect to product routes.
 server.use("/api/products",jwtAuth,productRouter);
@@ -26,6 +37,11 @@ server.use("/api/cartItems",jwtAuth, CartRouter);
 server.get("/",(req,res)=>{
     res.send("welcome to Ecommerce APIs");
 });
+
+//Middleware to handle 404 requests.
+server.use((req,res)=>{
+    res.status(404).send("API not found. Please check our documentation for more information at http://localhost:3200/api-docs")
+})
 
 server.listen(3200, ()=>{
     console.log("Server is running as 3200");
