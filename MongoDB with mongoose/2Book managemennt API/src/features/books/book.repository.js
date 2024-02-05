3bcf456d82d6dee40e41fb596b9c85e127d0f624
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-02-05 13:44:19 +0530

    updated
    updated

---
+++ MongoDB with mongoose/2Book managemennt API/src/features/books/book.repository.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { bookSchema } from './book.schema.js'
+
+
+const booksModel = mongoose.model('books',bookSchema)
+export default class BookRepository {
+
+
+    // -----Change code in below functions only-----
+
+    //book creation
+    async createBook(bookData) {
+        const book = new booksModel(bookData);
+        const savedBook = await book.save();
+        return savedBook;
+    }
+
+    //filtering the book by id
+    async getOne(id) {
+        const book = await booksModel.findById(id);
+        return book;
+    }
+    
+}
