dc3a988779be025655a17d4681949da3538b767d
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-02-07 15:39:43 +0530

    updated
    updated

--- E-comm Project using REST API with MongoDB/src/features/like/like.routes.js
+++ E-comm Project using REST API with MongoDB/src/features/like/like.routes.js
@@ -9,4 +9,7 @@ likeRouter.post("/", (req, res, next)=>{
     likeController.likeItem(req, res, next);
 })
 
+likeRouter.get("/", (req, res, next)=>{
+    likeController.getLikes(req, res, next);
+})
 export default likeRouter;
--- E-comm Project using REST API with MongoDB/src/features/like/like.schema.js
+++ E-comm Project using REST API with MongoDB/src/features/like/like.schema.js
@@ -13,4 +13,19 @@ export const likeSchema = new mongoose.Schema({
         type:String,
         enum:['Product','Category']
     }
+    
+    // Mongoose Middleware
+}).pre('save', (next)=>{
+    console.log("New like coming in");
+    next();
 })
+.post('save', (doc)=>{
+    console.log("Like is saved");
+    console.log(doc);
+}).pre('find', (next)=>{
+    console.log("Retriving likes");
+    next();
+}).post('find', (docs)=>{
+    console.log("Post find");
+    console.log(docs);
+});
