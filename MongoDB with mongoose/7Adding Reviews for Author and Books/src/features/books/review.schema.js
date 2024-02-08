427051349e2fe9a8b8a8de4c0a59eae9c6bf7e88
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-02-08 20:58:32 +0530

    updated
    updated

---
+++ MongoDB with mongoose/7Adding Reviews for Author and Books/src/features/books/review.schema.js
@@ -0,0 +1,27 @@
+
+// make necessary imports here
+import mongoose from "mongoose";
+
+
+// write your code here
+export const reviewSchema = new mongoose.Schema({
+    text:{
+        type:String,
+        required: true
+    },
+    rating: {
+        type:Number,
+        required: true,
+        min:1,
+        max:5,
+    },
+   
+    targetId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'target'
+    },
+    target:{
+        type: String,
+        enum:["Author", "Book"]
+    }
+})
